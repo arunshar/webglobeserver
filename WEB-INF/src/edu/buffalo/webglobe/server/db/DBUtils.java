44e828ff6eace09dedd1b20fe7c50d1802817233
chandola <chandola@buffalo.edu>
2017-04-15 17:35:06 -0400

    Pulling out database connection logic into DBUtils

--- WEB-INF/src/edu/buffalo/webglobe/server/db/DBUtils.java
+++ WEB-INF/src/edu/buffalo/webglobe/server/db/DBUtils.java
@@ -5,6 +5,7 @@ import edu.buffalo.webglobe.server.utils.Constants;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 /**
  * @author chandola
@@ -16,7 +17,7 @@ public class DBUtils {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("ALERT: WEBGLOBESERVER: "+Constants.DB_PASSWORD+" "+Constants.DB_USER_NAME);
+            System.err.println("ALERT: WEBGLOBESERVER: "+Constants.DB_PASSWORD+" "+Constants.DB_USER_NAME);
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webglobeserver", Constants.DB_USER_NAME, Constants.DB_PASSWORD);
         }  catch (SQLException ex) {
             ex.printStackTrace();
