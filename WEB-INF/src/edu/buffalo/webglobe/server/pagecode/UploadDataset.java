a9d5ba81907ad5596165583f7221b8dcbbd77890
chandola <chandola@buffalo.edu>
2017-04-18 10:01:10 -0400

    Adding code for processing images

--- WEB-INF/src/edu/buffalo/webglobe/server/pagecode/UploadDataset.java
+++ WEB-INF/src/edu/buffalo/webglobe/server/pagecode/UploadDataset.java
@@ -39,6 +39,12 @@ import java.util.logging.Logger;
 public class UploadDataset extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private Logger logger;
+    private String userName;
+    private String dataName;
+    private String hdfsURL;
+    private String dataInfo;
+    private String dataInfoURL;
+    private String responseData;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -63,103 +69,101 @@ public class UploadDataset extends HttpServlet {
 
         logger = Logger.getLogger("WEBGLOBE.LOGGER");
         JsonObject data = new Gson().fromJson(request.getReader(), JsonObject.class);
-        String hdfsURL = data.get("hdfsURL").getAsString();
-        String dataName = data.get("dataName").getAsString();
-        String dataInfo = data.get("dataInfo").getAsString();
-        String dataInfoURL = data.get("dataInfoURL").getAsString();
+        this.hdfsURL = data.get("hdfsURL").getAsString();
+        this.dataName = data.get("dataName").getAsString();
+        this.dataInfo = data.get("dataInfo").getAsString();
+        this.dataInfoURL = data.get("dataInfoURL").getAsString();
         Map<String, String> responseData = new HashMap<String, String>();
-        String userName = request.getUserPrincipal().getName();
-
-        responseData = this.uploadDataset(userName,dataName,hdfsURL,dataInfo,dataInfoURL,responseData);
+        this.userName = request.getUserPrincipal().getName();
+        //TODO - add submitted job info to database
+        //get a current job id
+        Thread loadingThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                uploadDataset();
+            }
+        });
+        loadingThread.start();
+        responseData.put("message", "Loading job started. Status of job is available under the user information panel. On success, the data set will be available under the climate data sets panel.");
         String responseJson = new Gson().toJson(responseData);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(responseJson);
     }
 
-    public Map<String,String> uploadDataset(String userName, String dataName, String hdfsURL, String dataInfo, String dataInfoURL, Map<String, String> responseData){
+    public void uploadDataset(){
         // query database
         Connection conn;
         Statement stmt;
         ResultSet rset;
         String message;
-        String status;
+        int status = 0;
         try {
             conn = DBUtils.getConnection();
             stmt = conn.createStatement();
-            String cmd = "select * from netcdf_datasets where name = '"+dataName+"' AND url = '"+hdfsURL+"'";
+            String cmd = "select * from netcdf_datasets where name = '" + dataName + "' AND url = '" + hdfsURL + "'";
             rset = stmt.executeQuery(cmd);
-            while(rset.next()){
+            while (rset.next()) {
                 message = "Error: Dataset already exists in database";
-                status = "-1";
-                responseData.put("message", message);
-                responseData.put("status", status);
-                return responseData;
+                status = -1;
+                break;
             }
             //open netcdfdata directory and find all Geogrid variables
+            if (status != -1) {
+                NetcdfDirNoVar ncDir = null;
+                try {
+                    ncDir = new NetcdfDirNoVar(hdfsURL);
+                    if (ncDir.getVariables() == null) {
+                        message = "Error: Unable to parse server address.";
+                        status = -1;
+                    } else {
+                        for (String variable : ncDir.getVariables()) {
+                            NetcdfDir netcdfDir = new NetcdfDir(hdfsURL, variable);
+                            String saveDir = netcdfDir.getDir() + "/variable/" + netcdfDir.getVariableName();
+                            File folder = new File(Constants.LOCAL_DIRECTORY + saveDir);
+                            folder.mkdirs();
+                            logger.info("STARTING IMAGE CREATION PROCESS");
+                            int startIndex = 0;
+                            int endIndex = netcdfDir.getFilepaths().size() * netcdfDir.getTimeLen() - 1;
+                            for (int i = startIndex; i <= endIndex; ++i) {
+                                Array src = netcdfDir.getData(i);
+                                float[][] data = ((float[][][]) src.copyToNDJavaArray())[0];
+                                MAMath.MinMax minmax;
+                                //TODO - Change this to accept minmax ranges from the user
+                                if (netcdfDir.getVariableName().equals("tasmax")) {
+                                    minmax = new MAMath.MinMax(200, 373);
+                                } else if (netcdfDir.getVariableName().equals("ChangeDetection")) {
+                                    minmax = new MAMath.MinMax(-1, 2);
+                                } else {
+                                    minmax = MAMath.getMinMax(src);
+                                }
 
-            NetcdfDirNoVar ncDir = null;
-            try {
-                ncDir = new NetcdfDirNoVar(hdfsURL);
-                if(ncDir.getVariables() == null){
-                    message = "Error: Unable to parse server address.";
-                    status = "-1";
-                    responseData.put("message", message);
-                    responseData.put("status", status);
-                    return responseData;
-                }
-                for(String variable: ncDir.getVariables()){
-                    NetcdfDir netcdfDir = new NetcdfDir(hdfsURL,variable);
-                    String saveDir = netcdfDir.getDir() + "/variable/" + netcdfDir.getVariableName();
-                    File folder = new File(Constants.LOCAL_DIRECTORY + saveDir);
-                    folder.mkdirs();
-                    logger.info("STARTING PROCESS");
-                    int startIndex = 0;
-                    int endIndex = netcdfDir.getFilepaths().size()*netcdfDir.getTimeLen()-1;
-                    for (int i = startIndex; i <= endIndex; ++i) {
-                        Array src = netcdfDir.getData(i);
-                        float[][] data = ((float[][][]) src.copyToNDJavaArray())[0];
-                        MAMath.MinMax minmax;
-                        //TODO - Change this to accept minmax ranges from the user
-                        if (netcdfDir.getVariableName().equals("tasmax")) {
-                            minmax = new MAMath.MinMax(200, 373);
-                        } else if (netcdfDir.getVariableName().equals("ChangeDetection")) {
-                            minmax = new MAMath.MinMax(-1, 2);
-                        } else {
-                            minmax = MAMath.getMinMax(src);
+                                logger.info("Creating image with index = " + i + "in " + Constants.LOCAL_DIRECTORY + saveDir + "/" + netcdfDir.getDateFromIndex(i) + ".png");
+                                Utils.createImage(data, (float) minmax.min, (float) minmax.max, Constants.LOCAL_DIRECTORY + saveDir + "/" + netcdfDir.getDateFromIndex(i) + ".png");
+                                logger.info("Created image with index = " + i + "in " + Constants.LOCAL_DIRECTORY + saveDir + "/" + netcdfDir.getDateFromIndex(i) + ".png");
+
+                            }
+                            logger.info("ENDED IMAGE CREATION PROCESS");
+                            message = "Data registration and Image loading successful";
+                            status = 1;
+                            //TODO - update netcdfdatasets and netcdfdataset_fields table
+                            //TODO - update netcdfdataset_images table
                         }
-
-                        logger.info("Creating image with index = "+i+ "in "+Constants.LOCAL_DIRECTORY + saveDir + "/" + netcdfDir.getDateFromIndex(i)+".png");
-                        Utils.createImage(data, (float) minmax.min, (float) minmax.max, Constants.LOCAL_DIRECTORY + saveDir + "/" + netcdfDir.getDateFromIndex(i) + ".png");
-                        logger.info("Created image with index = "+i+ "in "+Constants.LOCAL_DIRECTORY + saveDir + "/" + netcdfDir.getDateFromIndex(i)+".png");
-
                     }
-                    logger.info("ENDED PROCESS");
-                    // create Images
-
+                } catch (Exception e) {
+                    logger.log(Level.SEVERE, "Message", e);
+                    message = "Error: Unable to open HDFS file.";
+                    status = -1;
                 }
-            }catch(Exception e){
-                logger.log(Level.SEVERE,"Message",e);
-                message = "Error: Unable to open HDFS file.";
-                status = "-1";
-                responseData.put("message", message);
-                responseData.put("status", status);
-                return responseData;
+
             }
-            responseData.put("message","Found "+ncDir.getVariables().size()+" variables.");
-            responseData.put("status","1");
         }
         catch(SQLException e){
             message = "Error: Unable to connect to the database";
-            status = "-1";
-            responseData.put("message", message);
-            responseData.put("status", status);
-            return responseData;
-       }
-        //4. - find the geogrid variables
-        //5. - Create images
-        //6. - Enter details into DB (
-       return responseData;
+            status = -1;
+        }
+        //TODO - update submitted jobs table using message and status
+
     }
 
 }
