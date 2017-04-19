eddaaa8a0a1cd3e0dd98d78aca71a72fcdd693ca
chandola <chandola@buffalo.edu>
2017-04-19 19:26:02 -0400

    a

--- js/layer/NetcdfLayer.js
+++ js/layer/NetcdfLayer.js
@@ -20,13 +20,13 @@ define(['worldwind'],
 
             this.pickEnabled = false;
             this.minActiveAltitude = 3e6;
-	    this.layers = []; 
+	    self.layers = []; 
 	    self.populate = function(imgUrls){
 	      for(var i = 0; i < imgUrls.length; i++){
 		var surfaceImage = new WorldWind.SurfaceImage(WorldWind.Sector.FULL_SPHERE,imgUrls[i]);
 		var layer = new WorldWind.RenderableLayer("NETCDFLAYER");
 	        layer.addRenderable(surfaceImage);	
-		this.layers.add(layer);
+		self.layers.add(layer);
 	      }
 	    }
 	}
