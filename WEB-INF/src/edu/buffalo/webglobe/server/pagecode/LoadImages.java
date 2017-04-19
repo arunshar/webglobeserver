fdc2d3f123f355a1db1b45660e371f06bce30612
chandola <chandola@buffalo.edu>
2017-04-19 12:44:14 -0400

    New UploadDataset code

--- WEB-INF/src/edu/buffalo/webglobe/server/pagecode/LoadImages.java
+++ WEB-INF/src/edu/buffalo/webglobe/server/pagecode/LoadImages.java
@@ -53,16 +53,13 @@ public class LoadImages extends HttpServlet {
 		String to = request.getParameter("to");
 		ArrayList<String> imageUrls = new ArrayList<String>();
         ArrayList<String> imageDates = new ArrayList<String>();
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 		HashMap<String, ArrayList<String>> responseData = new HashMap<String, ArrayList<String>>();
 		try {
-			String startDate = dateFormatter.parse(from).toString();
-			String endDate = dateFormatter.parse(to).toString();
 			Connection conn = DBUtils.getConnection();
             Statement stmt = conn.createStatement();
             String cmd = "SELECT D.timestamp,D.time_index from netcdf_dataset_images as D and netcdf_dataset_fields as F where D.dataset_id = "+
                     datasetId+" and D.field_id = F.field_id and F.field_name=\""+fieldName+
-                    "\" and str_to_date(D.timestamp,'%Y-%m-%d') >= "+startDate+" and str_to_date(D.timestamp,'%Y-%m-%d') <= "+endDate;
+                    "\" and str_to_date(D.timestamp,'%Y-%m-%d') >= "+from+" and str_to_date(D.timestamp,'%Y-%m-%d') <= "+to;
             ResultSet rset = DBUtils.executeQuery(conn,stmt,cmd);
             while(rset.next()){
                 imageDates.add(rset.getDate(1).toString());
@@ -74,9 +71,6 @@ public class LoadImages extends HttpServlet {
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
             response.getWriter().write(responseJson);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (SQLException e) {
             e.printStackTrace();
         }
