49582cecceef0fafd3c42e7f84098a482ff03ffb
chandola <chandola@buffalo.edu>
2017-04-19 16:53:46 -0400

    ADding Sector info

--- js/layer/NetcdfLayer.js
+++ js/layer/NetcdfLayer.js
@@ -11,7 +11,7 @@ define(['worldwind'],
         var NetcdfLayer = function () {
             WorldWind.RenderableLayer.call(this, "NetcdfLayer");
 
-            var surfaceImage = new WorldWind.SurfaceImage(Sector.FULL_SPHERE,
+            var surfaceImage = new WorldWind.SurfaceImage(WorldWind.Sector.FULL_SPHERE,
                 "http://199.109.195.187:8000//user/ubuntu/BCSD/tasmax/variable/tasmax/2014-01-29.png");
 
             this.addRenderable(surfaceImage);
