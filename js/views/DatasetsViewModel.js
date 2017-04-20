ec34405531b67702ec3a4a8f0002b9bc73d4c333
chandola <chandola@buffalo.edu>
2017-04-20 16:23:51 -0400

    Minor edits

--- js/views/DatasetsViewModel.js
+++ js/views/DatasetsViewModel.js
@@ -373,8 +373,7 @@ define(
 	self.populateDatasets();
 
 	self.plotChart = function(lat,lon){
-	  var el = document.getElementById('innerChart');
-	  Plotly.plot(el, [{
+	  Plotly.plot($('#innerChart'), [{
 	    x: [1, 2, 3, 4, 5],
 	    y: [1, 2, 4, 8, 16] }], {
 	      margin: { t: 0 } } );
