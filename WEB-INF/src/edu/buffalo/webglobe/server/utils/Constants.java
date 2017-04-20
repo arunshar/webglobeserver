078dffcf31c0c30b3f09d7440693ab3f708cc1d8
chandola <chandola@buffalo.edu>
2017-04-20 08:15:22 -0400

    New Analysis code

--- WEB-INF/src/edu/buffalo/webglobe/server/utils/Constants.java
+++ WEB-INF/src/edu/buffalo/webglobe/server/utils/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 	public static final String PUBLIC_ADDRESS = "http://199.109.195.187:8000";
 	public static final String LOCAL_DIRECTORY = "/home/ubuntu/fileserver";
     public static final String DB_USER_NAME = "root";
-    public static final String DB_PASSWORD = "root";
+    public static final String DB_PASSWORD = "mysql";
 }
