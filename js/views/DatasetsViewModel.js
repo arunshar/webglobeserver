cd9a907aa6cf34326c0bc83b44bd3d61ff2ca6ae
chandola <chandola@buffalo.edu>
2017-04-20 22:28:04 -0400

    Minor edits

--- js/views/DatasetsViewModel.js
+++ js/views/DatasetsViewModel.js
@@ -399,7 +399,7 @@ define(
 	      }
 	      
 	      var xlabel = "Time";
-	      xdata = ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'];
+	      xdata = ['2013-10-04', '2013-11-04', '2013-12-04'];
 		ydata = [1,4,9];
 	      var data = {x:xdata,y:ydata,type:'scatter'};
 	      var layout = {
