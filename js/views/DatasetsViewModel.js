2a82266019ceda675e3293a396c87b632f7da698
chandola <chandola@buffalo.edu>
2017-04-23 11:37:36 -0400

    ui updates

--- js/views/DatasetsViewModel.js
+++ js/views/DatasetsViewModel.js
@@ -369,6 +369,7 @@ define(
 	      hdfsURL: hdfsURL,
 	    }),
 	    success: function (data) {
+	      alert(data.status);
 	      if(data.status == -1){
 		logger.log("Error loading data set","alert-danger");
 	      }
