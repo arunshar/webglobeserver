ff80159db60c86d115e54b6c77b3f4c8726bd42e
chandola <chandola@buffalo.edu>
2017-04-29 23:37:28 -0400

    Modified logging routine

--- WEB-INF/src/edu/buffalo/webglobe/server/db/GetDatasetDetails.java
+++ WEB-INF/src/edu/buffalo/webglobe/server/db/GetDatasetDetails.java
@@ -12,6 +12,8 @@ import java.lang.Integer;
 
 import com.google.gson.Gson;
 import edu.buffalo.webglobe.server.utils.Utils;
+import ucar.nc2.time.CalendarDate;
+import ucar.nc2.time.CalendarDateFormatter;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -63,7 +65,7 @@ public class GetDatasetDetails extends HttpServlet {
 			stmt = conn.createStatement();
 			String cmd = "select id,name,url,available,info,info_url,is_analyzable from netcdf_datasets where is_accessible = 1 and (available = \"all\" or available = \""
 					+ userName + "\")";
-			rset = stmt.executeQuery(cmd);
+			rset = DBUtils.executeQuery(conn,stmt,cmd);
 			int i = 0;
 			while (rset.next()) {
 				Map<String, String> datasetInfo = new HashMap<String, String>();
@@ -82,13 +84,38 @@ public class GetDatasetDetails extends HttpServlet {
 				datasetInfo.put("info_url", info_url);
                 datasetInfo.put("is_analyzable", Integer.toString(is_analyzable));
 				// get fields
-				String cmd1 = "select field_name from netcdf_dataset_fields where dataset_id = " + id;
+				String cmd1 = "select id,field_name from netcdf_dataset_fields where dataset_id = " + id;
 				stmt1 = conn.createStatement();
-				rset1 = stmt1.executeQuery(cmd1);
+				rset1 = DBUtils.executeQuery(conn,stmt1,cmd1);
 				int j = 0;
 				while (rset1.next()) {
 					datasetInfo.put("field" + j, rset1.getString("field_name"));
-					j++;
+                    // get time limits
+                    CalendarDateFormatter dateFormatter = new CalendarDateFormatter("yyyy-MM-dd");
+
+                    int fieldId = rset.getInt("id");
+                    String cmd2;
+                    Statement stmt2;
+                    ResultSet rset2;
+                    cmd2 = "select timestamp from netcdf_dataset_images where dataset_id="+id+
+                            " and field_id="+fieldId+" and time_index = (select min(time_index) from netcdf_dataset_images where dataset_id="+id+" and field_id="+fieldId+")";
+                    stmt2 = conn.createStatement();
+                    rset2 = DBUtils.executeQuery(conn,stmt2,cmd2);
+                    rset2.next();
+                    String tsmin = rset.getString(1);
+
+                    cmd2 = "select timestamp from netcdf_dataset_images where dataset_id="+id+
+                            " and field_id="+fieldId+" and time_index = (select max(time_index) from netcdf_dataset_images where dataset_id="+id+" and field_id="+fieldId+")";
+                    stmt2 = conn.createStatement();
+                    rset2 = DBUtils.executeQuery(conn,stmt2,cmd2);
+                    rset2.next();
+                    String tsmax = rset.getString(1);
+
+                    CalendarDate d1 = dateFormatter.parse(tsmin);
+                    CalendarDate d2 = dateFormatter.parse(tsmax);
+                    datasetInfo.put("field"+j+"_mindate", dateFormatter.toString(d1));
+                    datasetInfo.put("field"+j+"_maxdate", dateFormatter.toString(d2));
+                    j++;
 				}
 				datasetInfo.put("fieldcount", (new Integer(j)).toString());
 				responseData.put("dataset" + i, datasetInfo);
