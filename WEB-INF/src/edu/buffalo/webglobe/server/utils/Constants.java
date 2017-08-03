0a1fd4450580baa4d26e719078f2d95fc97c326b
chandola <chandola@buffalo.edu>
2017-08-03 18:00:46 -0400

    Comment added to Constants.java

--- WEB-INF/src/edu/buffalo/webglobe/server/utils/Constants.java
+++ WEB-INF/src/edu/buffalo/webglobe/server/utils/Constants.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
+  //all configuration elements here
   public static final List<String> VALID_EXTENSIONS = Arrays.asList("nc", "h5", "netcdf", "hdf", "grib", "grib2");
   public static final List<String> VALID_DESCRIPTION_TAGS = Arrays.asList("info",  "information", "description", "about");
   public static final List<String> VALID_TITLE_TAGS = Arrays.asList("title", "name");
